cb53dc08a960fb1af1cba943008295860525ba40
felipeaagoncalves <fagoncal@ualberta.ca>
2018-05-30 10:40:12 +0200

    Delete OPF_test1.py

--- tkinter/OPF_test1.py
+++
@@ -1,6 +0,0 @@
-from tkinter import messagebox
-import datetime as dt
-
-# today = dt.datetime.now().strftime("%H:%M")
-# messagebox.showinfo(title="Now it is:", message=today)
-messagebox.showerror(title="Error", message="No data yet.")
